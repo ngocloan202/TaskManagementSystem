2d66bc76a8f9e7f4f87130678e8b72f925c3fe15
ngocloan202 <chauthingocloan0309@gmail.com>
2025-05-05 22:18:34 +0700

    initialize project with package.json, package-lock.json, and Tailwind CSS setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: [
+      "./app/Views/**/*.php",
+      "./public/js/**/*.js",
+      "./frontend/css/**/*.css"
+    ],
+    theme: { 
+      extend: {} 
+    },
+    plugins: [],
+  }
+  
