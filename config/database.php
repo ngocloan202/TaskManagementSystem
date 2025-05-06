d2350951f323ce6255f7421911697e1345efabe3
Nguyen Quan <ngquan6704@gmail.com>
2025-05-06 10:45:57 +0700

    fix: correct indentation for server name variable in database configuration

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
     header("Content-Type: text/html; charset=utf-8");
-	$servername = "localhost";
+    $servername = "localhost";
     $username = "root";
     $password = "vertrigo";
     $dbname = "TaskManagementSystem";
