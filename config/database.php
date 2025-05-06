11970a945ab766001b002c60437cee86351baaaf
Nguyen Quan <ngquan6704@gmail.com>
2025-05-06 10:16:43 +0700

    add database config

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+    header("Content-Type: text/html; charset=utf-8");
+	$servername = "localhost";
+    $username = "root";
+    $password = "vertrigo";
+    $dbname = "TaskManagementSystem";
+    $connect = mysqli_connect($servername, $username, $password, $dbname);
+    mysqli_set_charset($connect, "utf8");
+    if ($connect -> connect_error) {
+        die("Không thể kết nối " . $connect -> connect_error);
+        exit();
+    }
+?>
